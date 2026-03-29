c812f8fb9bda3f86dbfd75de5573520ea3f0344a
Harvey Zhao <laozhaochaguan@gmail.com>
2026-03-29 19:49:30 +0800

    style: format code for consistency and readability in IconSymbol component

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -1,11 +1,14 @@
 // Fallback for using MaterialIcons on Android and web.
 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
-import { ComponentProps } from 'react';
-import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { SymbolViewProps, SymbolWeight } from "expo-symbols";
+import { ComponentProps } from "react";
+import { OpaqueColorValue, type StyleProp, type TextStyle } from "react-native";
 
-type IconMapping = Record<SymbolViewProps['name'], ComponentProps<typeof MaterialIcons>['name']>;
+type IconMapping = Record<
+  SymbolViewProps["name"],
+  ComponentProps<typeof MaterialIcons>["name"]
+>;
 type IconSymbolName = keyof typeof MAPPING;
 
 /**
@@ -15,14 +18,14 @@ type IconSymbolName = keyof typeof MAPPING;
  */
 const MAPPING = {
   // Tabs
-  'house.fill': 'home',
-  'safari.fill': 'explore',
-  'message.fill': 'chat-bubble',
-  'person.fill': 'person',
+  "house.fill": "home",
+  "safari.fill": "explore",
+  "message.fill": "chat-bubble",
+  "person.fill": "person",
   // Others
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
+  "paperplane.fill": "send",
+  "chevron.left.forwardslash.chevron.right": "code",
+  "chevron.right": "chevron-right",
 } as IconMapping;
 
 /**
@@ -42,5 +45,12 @@ export function IconSymbol({
   style?: StyleProp<TextStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return (
+    <MaterialIcons
+      color={color}
+      size={size}
+      name={MAPPING[name]}
+      style={style}
+    />
+  );
 }
