4b552b2156bd0b888c51c39920680658495d3763
Harvey Zhao <laozhaochaguan@gmail.com>
2026-03-29 21:12:51 +0800

    chore: update VSCode settings for Tailwind CSS and remove unused screen paths from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
