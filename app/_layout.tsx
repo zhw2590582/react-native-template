3ad62257bd056800d2c3b2a816d37d63db1bdbed
Harvey Zhao <laozhaochaguan@gmail.com>
2026-03-31 12:47:52 +0800

    refactor: remove modal screen and its associated stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,10 +37,6 @@ export default function RootLayout() {
         >
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="modal"
-              options={{ presentation: "modal", title: "Modal" }}
-            />
           </Stack>
           <StatusBar style="auto" />
           <Toaster theme={colorScheme === "dark" ? "dark" : "light"} />
